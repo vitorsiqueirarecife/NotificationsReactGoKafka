dcd3162da0f0459847e6a7a6d39a642ce5422e7f
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-02-10 18:09:07 -0300

    fixed model mistake

--- backend/bff/model/message.go
+++ backend/bff/model/message.go
@@ -3,5 +3,5 @@ package model
 type Message struct {
 	CategoryID string `json:"category_id" validate:"required"`
 	Text       string `json:"text" validate:"required"`
-	Target     string `json:"text"`
+	Target     string `json:"target"`
 }
