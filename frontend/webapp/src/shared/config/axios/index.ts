57d8f925b9950bd5530f24554f218e904545577f
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-02-11 13:37:31 -0300

    set Content-Type

--- frontend/webapp/src/shared/config/axios/index.ts
+++ frontend/webapp/src/shared/config/axios/index.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const apiV1 = axios.create({
-  baseURL: 'http://127.0.0.1:4000/api/v1',  
+  baseURL: 'http://127.0.0.1:4000/api/v1',
+  headers: {
+    'Content-Type': 'application/json'
+  }  
 });
 
 export default apiV1;
